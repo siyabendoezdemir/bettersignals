9458d55ca88f0eaa65d90aee4193484ebb81e24d
OESI2 <siyabend.oezdemir@coop.ch>
2023-10-19 17:23:53 +0200

    logged in Users can now subscribe to Traders using the /subscribe/:traderId endpoint

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Trader = require("../models/trader");
 
 exports.getUsers = (req, res) => {
   User.find({})
@@ -50,4 +51,45 @@ exports.getDashboard = (req, res) => {
   res.render("dashboard", { user: req.user });
 };
 
-exports.subscribe = (req, res) => {};
+exports.subscribe = async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const user = req.user;
+  const userId = req.user._id; // Get the user's ID
+  const traderId = req.params.traderId; // Get the trader's ID
+
+  try {
+    // Check if the trader exists
+    const trader = await Trader.findById(traderId);
+    if (!trader) {
+      return res.status(404).json({ error: "Trader not found" });
+    }
+
+    // Check if the user is already subscribed to this trader
+    const isSubscribed = user.subscribedTraders.some((sub) =>
+      sub.equals(traderId)
+    );
+    if (isSubscribed) {
+      return res
+        .status(400)
+        .json({ error: "You are already subscribed to this trader" });
+    }
+
+    // Add the trader to the user's list of subscribed traders
+    user.subscribedTraders.push(traderId);
+    await user.save();
+
+    // Update the trader's list of subscribers
+    trader.subscribers.push(userId);
+    await trader.save();
+
+    res.status(200).json({ message: "Subscribed to the trader successfully" });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing your request" });
+  }
+};
